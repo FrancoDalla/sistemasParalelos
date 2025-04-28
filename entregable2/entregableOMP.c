950038806dc397221c72363faf9e6de634b91ea0
Francodalla <francodalladg@outlook.com>
2025-04-28 13:03:32 -0300

    a

--- entregable2/entregableOMP.c
+++ entregable2/entregableOMP.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <float.h>
 #include <omp.h>
+
+#define DBL_MAX 1.7976931348623157e+308
+#define DBL_MIN 2.2250738585072014e-308
 
 double dwalltime();
 void matmulblks_and_calculate_scalar(double *a, double *b, double *c);
@@ -11,32 +13,36 @@ void transpose(double *m, double *mt);
 void matmulblks(double *a, double *b, double *c);
 void blkmul(double *ablk, double *bblk, double *cblk);
 
+
+// Como se nos indicó, declaramos variables globales para evitar usar parámetros.
 int n = 0, bs = 32;
 double prom_a = 0.0, prom_b = 0.0, escalar = 0.0;
-double max_a = -1, max_b = -1;
-double min_a = 32000, min_b = 32000;
+double max_a = DBL_MIN, max_b = DBL_MIN;
+double min_a = DBL_MAX, min_b = DBL_MAX;
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+
+    /*
+        Definición de variables.
+    */
+    int i = 0, j = 0, k = 0, ii = 0, jj = 0, kk = 0;
     double timetick;
     int verificacion = 0;
-    int i,j,t,k;
 
-    /*Verificacion del input */
-    if(argc < 3 || argc > 5){
+    if(argc < 2 || argc > 4){
         fprintf(stderr, "Uso %s N [BS] [verificacion]\n", argv[0]);
         fprintf(stderr, "N: Tamaño matriz.\n");
-        fprintf(stderr, "T: Cantidad de hilos a utilizar\n");
         fprintf(stderr, "BS: Tamaño de los bloques, por defecto 32(x32).\n");
         fprintf(stderr, "verificacion: Si es 1 o mayor activa la verificación de los calculos.\n");
         exit(1);
     } else {
         n = atoi(argv[1]);
-        t = atoi(argv[2]);
-        if (argc >= 4)
-            bs = atoi(argv[3]);
-        if (argc == 5)
-            verificacion = atoi(argv[4]);
+        if (argc >= 3)
+            bs = atoi(argv[2]);
+        if (argc == 4)
+            verificacion = atoi(argv[3]);
     }
+    // Verificación de input
 
     if (n<=0) {
         fprintf(stderr, "N debe ser mayor o igual a 0.");
@@ -51,7 +57,6 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
-    /*Reserva de memoria para matrices */
     double * a = malloc(n*n * sizeof(double));
     double * b = malloc(n*n * sizeof(double));
     double * c = malloc(n*n * sizeof(double));
@@ -60,8 +65,13 @@ int main(int argc, char *argv[]){
     double * matriz_ab = malloc(n*n * sizeof(double));
     double * matriz_cbt = malloc(n*n * sizeof(double));
 
-    omp_set_num_threads(t);
-    /*Inicializacion de matrices */
+    /*Pongo el valor de la cantidad de hilos, convertir a pasaje por parametro */
+    int num_threads = 4;
+    omp_set_num_threads(num_threads);
+
+    /*
+        Inicialización de matrices.
+    */
     for(i = 0; i < n; i++){
         for(j = 0;j < n;j++){
             a[i*n+j] = j;
@@ -76,32 +86,41 @@ int main(int argc, char *argv[]){
     timetick = dwalltime();
 
     /*
-    escalar = [(maxA * maxB - minA * minB) / (promA * promB)]
-    R = (escalar * [A*B] + [C * bt])
+        escalar = [(MaxA * MaxB - MinA * MinB) / (PromA * PromB)].
+        R = ( escalar * [A*B] + [C*B_t].
     */
+
+    // Cálculo de A*B + Obtención de MinA, MinB, MaxA, MaxB, PromA, PromB.
     #pragma omp parallel
     {
-        matmulblks_and_calculate_scalar(a, b, c);
-        transpose(b, b_trans);
-        matmulblks(c,b_trans, matriz_cbt);
+        matmulblks_and_calculate_scalar(a,b,matriz_ab);
 
+        #pragma omp barrier
+
+        #pragma omp single
+        {
+            prom_a = prom_a / (n*n);
+            prom_b = prom_b / (n*n);
+            escalar = (max_a * max_b - min_a * min_b) / (prom_a * prom_b);
+        }
+
+        // Calcular la transposición de B
+        transpose(b, b_trans);
+
+        // Cálculo de C*B_t.
+        matmulblks(c, b_trans, matriz_cbt);
+
+        // Cálculo de R.
         for(i = 0; i < n; i++){
             for(j = 0; j < n; j++){
                 r[i*n+j] = escalar * matriz_ab[i*n+j] + matriz_cbt[i*n+j];
             }
         }
     }
-
-
     timetick = dwalltime() - timetick;
 
-    for(int i = 0; i < n*n; i++){
-        printf("%g ",r[i]);
-    }
-    printf("\n");
-
     /*
-        Verificación de resultados.
+        Verificación de resultados por método lento.
     */
     if (verificacion >= 1) {
         // Por se incializan los valores, el valor escalar siempre es 4.
@@ -144,9 +163,7 @@ int main(int argc, char *argv[]){
     free(matriz_cbt);
 
     return 0;
-
 }
-
 
 /* Multiplicación de matrices utilizando bloques. Para A x B y conseguir el valor escalar. */
 void matmulblks_and_calculate_scalar(double *a, double *b, double *c){
@@ -158,9 +175,6 @@ void matmulblks_and_calculate_scalar(double *a, double *b, double *c){
             }
         }
     }
-    prom_a = prom_a / (n*n);
-    prom_b = prom_b / (n*n);
-    escalar = (max_a * max_b - min_a * min_b) / (prom_a * prom_b);
 }
 
 /* Multiplicación de los bloques y obtener valores para el valor escalar */
@@ -169,20 +183,25 @@ void blkmul_getscalar(double *ablk, double *bblk, double *cblk, int first_row, i
     for (i = 0; i < bs; i++) {
         for (j = 0; j < bs; j++){
             // Obtención de MinA, MaxA, PromA
-            if (first_row) {
-                double val_a = ablk[i*n+j];
-                if (val_a > max_a) max_a = val_a;
-                if (val_a < min_a) min_a = val_a;
-                prom_a += val_a;
-            }
-            // Obtención de MinB, MaxB, PromB
-            if (first_column) {
-                double val_b = bblk[i+j*n];
-                if (val_b > max_b) max_b = val_b;
-                if (val_b < min_b) min_b = val_b;
-                prom_b += val_b;
-            }
-
+                #pragma omp critical
+                {
+                    if (first_row) {
+                        double val_a = ablk[i*n+j];
+                        if (val_a > max_a) max_a = val_a;
+                        if (val_a < min_a) min_a = val_a;
+                        prom_a += val_a;
+                    }
+                }
+                // Obtención de MinB, MaxB, PromB
+                #pragma omp critical
+                {
+                    if (first_column) {
+                        double val_b = bblk[i+j*n];
+                        if (val_b > max_b) max_b = val_b;
+                        if (val_b < min_b) min_b = val_b;
+                        prom_b += val_b;
+                    }
+                }
             for(k = 0; k < bs; k++){
                 // Calculo A*B
                 cblk[i*n+j] += ablk[i*n+k] * bblk[k+j*n];
